34a92304f839055d2b00750c47b70ba5fb9dc7b7
BSrivathsa <BSrivathsa@PRO-IND-1055.ad.prolifics.com>
2025-01-09 15:28:18 +0530

    "Initial commit"

---
+++ k8-demo/src/main/java/com/example/k8/demo/K8DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.k8.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+@SpringBootApplication
+
+public class K8DemoApplication {
+    
+	public static void main(String[] args) {
+		SpringApplication.run(K8DemoApplication.class, args);
+	}
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "You have deployed your application to kubernetes";
+	}
+
+}
---
+++ k8-demo/src/test/java/com/example/k8/demo/K8DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
